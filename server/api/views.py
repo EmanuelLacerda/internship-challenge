87d9c1f4a7896591fce965d6e45581898326523a
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2025-08-27 23:06:26 -0300

    feat(server/api/views.py): Add LCMIntervalCalculationAPIView

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import LCMIntervalCalculationSerializer
 
-# Create your views here.
+class LCMIntervalCalculationAPIView(APIView):
+    
+    def post(self, request):
+        serializer = LCMIntervalCalculationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
