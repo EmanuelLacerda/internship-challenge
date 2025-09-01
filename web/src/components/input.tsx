246a6eb7fd3b3a19b49474465ae86d0ea82acd52
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2025-08-31 23:20:36 -0300

    feat(web/src/components/input.tsx): Add error message container

--- web/src/components/input.tsx
+++ web/src/components/input.tsx
@@ -3,14 +3,18 @@ import { EnumTypeText, Text } from "./text";
 
 interface InputProps extends ComponentProps<"input"> {
     label?: string;
+    errorMessage?: string;
 }
 
-export function Input({label, className, ...props}: InputProps){
+export function Input({label, className, errorMessage, ...props}: InputProps){
     return <div className="input-field-container">
         {label && <Text className="label" type={EnumTypeText.label}>{label}</Text>}
         <input
             className={`element md ${className}`}
             {...props}
         />
+        <div className="error-message-container">
+            {errorMessage && <Text type={EnumTypeText.errorFeedbackRegular}>{`${errorMessage}`}</Text>}
+        </div>
     </div>
 }
