6159aa87eef51d11182d94eddcd10c73808db3f7
emanuel_lacerda <emanuelsouzalacerda@gmail.com>
2025-09-01 21:23:07 -0300

    feat(web/src/lcm_interval)/: Add API request management

--- web/src/lcm_interval/components/lcm_interval_form.tsx
+++ web/src/lcm_interval/components/lcm_interval_form.tsx
@@ -1,46 +1,54 @@
 import { useForm } from "react-hook-form";
 
-import { z } from "zod";
-
 import { Button } from "../../components/button";
 import { Input } from "../../components/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { EnumTypeText, Text } from "../../components/text";
 
-const commonNumberRule = z
-    .number({error: (iss) => iss.input === "" ? "Este campo é obrigatório." : undefined})
-    .refine(
-        val => !isNaN(val) && val > 0 && val <= 10000, "O valor deste campo deve ser um número inteiro entre 0 e 10.000!"
-    );
+import useLCMInterval from "../../hooks/use-lcm-interval"
+import { useState } from "react";
+import { formSchema, type IFormSchema, type IResponseSchema } from "../../lib/types";
+import type { AxiosResponse } from "axios";
 
-const formSchema = z
-    .object({
-        first_number:commonNumberRule,
-        last_number: commonNumberRule
-    })
-    .refine(data => data.first_number < data.last_number, {
-        message: 'O primeiro elemento do intervalo deve ser menor do que o último!',
-        path: ["last_number"]
-    })
-    .refine(data => data.last_number - data.first_number > 0, {
-        message: 'O tamanho do intervalo deve ser maior do que 0(zero)!',
-        path: ["last_number"]
-    })
-
-type IFormSchema = z.infer<typeof formSchema>;
 
 export default function LCMIntervalForm(){
+    const [ lcmResult, setLCMResult ] = useState<IResponseSchema|null>(null)
+    const [ requestErrors, setRequestErrors ] = useState<Array<string>|null>(null)
+
     const {
         register,
         handleSubmit,
         formState: { errors, isSubmitting },
-        reset
     } = useForm<IFormSchema>({
         resolver: zodResolver(formSchema),
     })
+    const {calculateLCMInterval} = useLCMInterval("lcm-interval-calculation/")
 
-    const onSubmit = async (data) => {
-        await new Promise(resolver => setTimeout(resolver, 1000))
-        reset();
+    const fieldMap = {
+        "first_number": "Primeiro Campo",
+        "last_number": "Último Campo"
+    }
+
+    const onSubmit = async (data: IFormSchema) => {
+        const response = await calculateLCMInterval(data)
+
+        if((response as AxiosResponse).status === 200){
+            setLCMResult({
+                ...(response as AxiosResponse).data
+            })
+        } else if((response as AxiosResponse).status === 400){
+            if(typeof (response as AxiosResponse).data === "string"){
+                setRequestErrors([(response as AxiosResponse).data])
+            } else if(typeof (response as AxiosResponse).data === "object"){
+                const requestErrorsArray = []
+
+                for(const error_name of Object.keys((response as AxiosResponse).data)){
+                    requestErrorsArray.push(error_name ? `${fieldMap[error_name as keyof typeof fieldMap]} ${(response as AxiosResponse).data[error_name][0].toLowerCase()}` : (response as AxiosResponse).data[error_name][0])
+                }
+
+                setRequestErrors(requestErrorsArray)
+            }
+        }
     }
 
     return <form
@@ -69,7 +77,18 @@ export default function LCMIntervalForm(){
             />
         </div>
         <div className="form-footer">
-            <Button disabled={isSubmitting}>CALCULAR</Button>
+            <Button disabled={isSubmitting} onClick={() => setLCMResult(null)}>CALCULAR</Button>
         </div>
+        <ul className="error-message-container" style={{"listStyleType": "none"}}>
+            {requestErrors?.map((
+                error, index) => <li key={`${index}-${error}`}>
+                    <Text type={EnumTypeText.errorFeedbackRegular}>{error}</Text>
+                </li>
+            )}
+        </ul>
+        {lcmResult && <div className="result_container">
+            <Text type={EnumTypeText.body1bold} as="span">O MMC dos números entre {lcmResult.first_number} e {lcmResult.last_number}:</Text>
+            <Text type={EnumTypeText.body1regular} as="span">{lcmResult.lcm}</Text>
+        </div>}
     </form>
 }
